d5a43354173760bdddd1bc96e2fec04d957b4f7f
joyce chan <joyce@selfcarecatalysts.com>
2015-11-13 23:07:34 +0000

    up

--- forum/spiders/cancer_cancerresearchuk_spider.py
+++ forum/spiders/cancer_cancerresearchuk_spider.py
@@ -52,3 +52,48 @@ class ForumsSpider(CrawlSpider):
             logging.info(item.__str__)
             items.append(item)
         return items
+
+
+# import scrapy
+# from scrapy.contrib.spiders import CrawlSpider, Rule
+# from scrapy.contrib.linkextractors import LinkExtractor
+# from scrapy.selector import Selector
+# from forum.items import PostItemsList
+# import re
+# import logging
+# from bs4 import BeautifulSoup
+# class ForumsSpider(CrawlSpider):
+#     name = "breastcancer_cancerresearchuk_spider"
+#     allowed_domains = ["cancerresearchuk.org"]
+#     start_urls = [
+#         "https://www.cancerresearchuk.org/about-cancer/cancer-chat/",
+#     ]
+
+#     rules = (
+#             Rule(LinkExtractor(
+#                     restrict_xpaths='//div[contains(@class,"table-list views-table cols-5")]/ol/li/div[1]/a[1]',
+#                 ), callback='parsePostsList'),
+#             Rule(LinkExtractor(
+#                     restrict_xpaths='//li[@class="next last"]',
+#                 ), follow=True),
+#         )
+#     def parsePostsList(self,response):
+#         sel = Selector(response)
+#         html = response.body
+#         soup = BeautifulSoup(html)
+#         users = soup.findAll('a',{'class':'username'})
+#         items = []
+#         topic = response.xpath('//h1/text()').extract()
+#         url = response.url
+#         for x in range(len(users)):
+#             item = PostItemsList()
+#             item['author'] = soup.findAll('a',{'class':'username'})[x].text
+#             item['author_link']=soup.findAll('a',{'class':'username'})[x]['href']
+#             item['create_date']= soup.findAll('div',{'class':'post-content-inner'})[x].span.text[0:11]
+#             item['post'] = soup.findAll('div',{'class':'post-content-inner'})[x].find('div',{'class':'field-item even'}).text
+#             item['tag']='cancer'
+#             item['topic'] = topic
+#             item['url']=url
+#             logging.info(item.__str__)
+#             items.append(item)
+#         return items
